c8e34df33824647ec0bbc16c786eaa8614523ada
olanikegloria github <olanikegloria2020@gmail.com>
2023-05-17 13:25:20 +0100

    form validation

--- validation.js
+++ validation.js
@@ -2,16 +2,12 @@ const email = document.getElementById('email');
 const form = document.getElementById('form');
 const message = document.querySelector('.message');
 
-form.addEventListener('submit', (e) => {   
-    const regex = /[A-Z]/;
-    if (regex.test(email.value)) {
-        e.preventDefault();
-        message.style.opacity = 1;
-    } else {
-        message.style.opacity = 0;
-    }
-
-    
-
-
-})
+form.addEventListener('submit', (e) => {
+  const regex = /[A-Z]/;
+  if (regex.test(email.value)) {
+    e.preventDefault();
+    message.style.opacity = 1;
+  } else {
+    message.style.opacity = 0;
+  }
+});
