fef30f2782342359995d7c7e112ee951f74a2d2e
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-05-17 14:08:22 +0300

    Add form validation

--- validation.js
+++ validation.js
@@ -1,8 +1,17 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
-const submitBtn = document.getElementById('submit-btn');
+const message = document.querySelector('.message');
 
-submitBtn.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener('submit', (e) => {   
+    const regex = /[A-Z]/;
+    if (regex.test(email.value)) {
+        e.preventDefault();
+        message.style.opacity = 1;
+    } else {
+        message.style.opacity = 0;
+    }
+
     
+
+
 })
